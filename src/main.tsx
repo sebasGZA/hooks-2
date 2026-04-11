38878ed923eae28eb12b08614931f9fb35161f65
sebasGZA <sebassga34@gmail.com>
2026-04-11 14:56:28 -0500

    feat: traffic light with useEffect

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect'
 // import HooksApp from './HooksApp'
-import { TrafficLight } from './useState/TrafficLight'
+// import { TrafficLight } from './useState/TrafficLight'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+    <TrafficLightWithEffect />
   </StrictMode>,
 )
