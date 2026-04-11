cb63a4d5ceec5f07437d6a7d7af0319d5c83209b
sebasGZA <sebassga34@gmail.com>
2026-04-11 13:41:42 -0500

    feat: set up tailwind css

--- src/HooksApp.tsx
+++ src/HooksApp.tsx
@@ -1,9 +1,9 @@
 const HooksApp = () => {
-  return (
-    <>
-      Hooks App
-    </>
-  )
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
 }
 
 export default HooksApp
