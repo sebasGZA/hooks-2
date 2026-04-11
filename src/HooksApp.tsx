23533c2af52cceac65d5df758544fc0ce5101243
sebasGZA <sebassga34@gmail.com>
2026-04-11 14:24:33 -0500

    feat: use state example

--- src/HooksApp.tsx
+++ src/HooksApp.tsx
@@ -1,8 +1,11 @@
 const HooksApp = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className="bg-gradient">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+
     )
 }
 
---
+++ src/useState/TrafficLight.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+type TrafficLightColor = 'red' | 'yellow' | 'green';
+
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+};
+
+export const TrafficLight = () => {
+
+    const [light, setLight] = useState<TrafficLightColor>('red')
+
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+            <div className="flex flex-col items-center space-y-8">
+                <div className={`w-32 h-32 ${light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+
+                <div className="flex gap-2">
+                    <button
+                        onClick={() => setLight('red')}
+                        name="red"
+                        className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        Rojo
+                    </button>
+                    <button
+                        onClick={() => setLight('yellow')}
+                        name="yellow"
+                        className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        Amarillo
+                    </button>
+                    <button
+                        onClick={() => setLight('green')}
+                        name="green"
+                        className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        Verde
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
